3c857c748d25718e12172697cfc859463a08a576
projmominho <projmominho@proton.me>
2025-05-16 12:19:14 -0300

    adiciona suporte a cors

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from cupcake import router as cupcake_router
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Obter as origens permitidas a partir da variável de ambiente
+allowed_origins = os.getenv("CORS_ALLOWED_ORIGINS").split(",")
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(cupcake_router)
